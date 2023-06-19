164c9b51b84d7d3836b8c119c4de771f77fa17d1
1006207745 <1006207745@qq.com>
2023-06-19 13:46:32 +0800

    1

---
+++ client/src/components/index.js
@@ -0,0 +1,9 @@
+import pageHOC from "./pageHOC";
+import CustomInput from "./CustomInput";
+import CustomButton from "./CustomButton";
+
+export {
+    pageHOC,
+    CustomInput,
+    CustomButton,
+}
---
+++ client/src/contract/index.js
@@ -0,0 +1,4 @@
+import contract from './AVAXGods.json';
+
+export const ADDRESS = '0x3F0ADFab40C61089B662125aeDa769df66df1587'
+export const { abi: ABI } = contract;
